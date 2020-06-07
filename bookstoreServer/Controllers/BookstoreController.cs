09d2fd4354ffaaad205abe058985e1689a4bf998
YuliaSincere <60033205+YuliaSincere@users.noreply.github.com>
2020-06-07 20:14:41 +0400

    Дополнение контроллера

--- bookstoreServer/Controllers/BookstoreController.cs
+++ bookstoreServer/Controllers/BookstoreController.cs
@@ -1,24 +1,43 @@
+using bookstoreServer.Database;
+using bookstoreServer.Database.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
 
 namespace bookstoreApp.Controllers
 {
-    [Route("api/b")]
+    [Route("api/store")]
     [ApiController]
     public class BookstoreController : Controller
     {
-        [HttpGet]
-        [Route("index")]
-        public string Index()
+        private readonly BookstoreDbContext _context;
+
+        public BookstoreController(BookstoreDbContext context)
         {
-            return "test1...";
+            _context = context;
         }
 
         [HttpGet]
-        //[Route("api/b/welcome")]
-        public string Welcome()
+        public IEnumerable<BookTest> GetBookTest()
         {
-            return "test2...";
+            return _context.Stores
+                .Include(s => s.Book)
+                .Select(s => ConvertToDto(s)).ToList();
+        }
+
+        static private BookTest ConvertToDto(Store s)
+        {
+            var result = new BookTest();
+
+            result.Description = s.Book.Description;
+            result.Count = s.Count;
+
+            return result;
         }
     }
 }
