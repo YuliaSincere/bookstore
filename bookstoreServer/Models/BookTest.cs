86c45654c122e6b73a616cb75c12490c2555599c
YuliaSincere <60033205+YuliaSincere@users.noreply.github.com>
2020-06-07 20:07:22 +0400

    Описание модели

---
+++ bookstoreServer/Models/BookTest.cs
@@ -0,0 +1,8 @@
+public class BookTest
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public int Count { get; set; }
+}
