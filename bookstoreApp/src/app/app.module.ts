9095b0258e2ed42a133461c63f3f5d2cff9363d9
YuliaSincere <60033205+YuliaSincere@users.noreply.github.com>
2020-06-11 10:44:58 +0400

    Добавление import, declaration и BookProvider в провайдеры

--- bookstoreApp/src/app/app.module.ts
+++ bookstoreApp/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { BookstoreComponent } from './bookstore/bookstore.component';
+import { BookProvider } from './Services/bookprovider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    BookstoreComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [BookProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
