b02adf8f99f0fa42a0466392480febd212b3c695
YuliaSincere <60033205+YuliaSincere@users.noreply.github.com>
2020-06-11 10:40:42 +0400

    Создание компонента списка доступных книг

---
+++ bookstoreApp/src/app/bookstore/bookstore.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core'
+import { BookProvider } from '../Services/bookprovider';
+import { Book } from '../Models/book';
+
+@Component({
+    selector: 'app-bookstore',
+    templateUrl: './bookstore.component.html',
+    styleUrls: ['./bookstore.component.scss']
+})
+
+export class BookstoreComponent {
+    private bookProvider: BookProvider; //this
+
+    constructor(bookProvider: BookProvider) {
+        this.bookProvider = bookProvider;
+    }
+
+    get books(): Book[] {
+        return this.bookProvider.books;
+    }
+}
