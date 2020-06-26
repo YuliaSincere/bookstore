176ca8ee9915c449a7a56078c7955b9799a93693
YuliaSincere <60033205+YuliaSincere@users.noreply.github.com>
2020-06-26 11:02:25 +0400

    Переименование транспортного класса Книги

---
+++ bookstoreServer/Models/BookDto.cs
@@ -0,0 +1,12 @@
+/// <summary>
+/// Транспортный класс для передачи информации о книге (количестве, остатке) для передачи на клиент.
+/// </summary>
+public class BookDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public int Count { get; set; }
+}
+
--- bookstoreServer/Models/BookTest.cs
+++
@@ -1,8 +0,0 @@
-public class BookTest
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public double Price { get; set; }
-    public int Count { get; set; }
-}
